c4e78b66b82ba6d2e51a064fc06d6fe44ee21cbf
codEnjoyer <ch4shkin.nikita@gmail.com>
2024-06-15 01:10:51 +0500

    upd: схемы бронирований

--- src/domain/schemas/booking.py
+++ src/domain/schemas/booking.py
@@ -1,31 +1,37 @@
-import datetime as dt
+import datetime
 import uuid
 
 from src.domain.schemas.base import BaseSchema
 from src.domain.models.booking import BookingStatus
+from src.domain.schemas.room import RoomRead
+from src.domain.schemas.user import UserRead
 
 
 class BaseBooking(BaseSchema):
     name: str
     surname: str | None
-    starts_at: dt.datetime
-    ends_at: dt.datetime
+
+
+class BookingRead(BaseBooking):
+    id: uuid.UUID
+    status: BookingStatus
+
+    starts_at: datetime.datetime
+    ends_at: datetime.datetime
+    # TODO: starts_at > ends_at
+    # TODO: check for minutes 00 or 30
+
+    user: UserRead
+    room: RoomRead
+
+
+class BookingCreate(BaseBooking):
+    starts_at: datetime.datetime
+    ends_at: datetime.datetime
 
     # TODO: starts_at > ends_at
     # TODO: check for minutes 00 or 30
 
 
-class BookingRead(BaseBooking):
-    id: uuid.UUID
-    user_id: int
-    status: BookingStatus
-    room_id: int
-
-
-class BookingCreate(BaseBooking): ...
-
-
-class BookingUpdate(BaseBooking): ...
-
-
-# TODO: оставить только name и surname?
+class BookingUpdate(BaseBooking):
+    ...

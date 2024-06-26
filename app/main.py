55a2eedc3265d617417695d00cb9c7602c8b1630
codEnjoyer <ch4shkin.nikita@gmail.com>
2024-06-27 00:45:29 +0500

    add: cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request, Response, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import v1_router
 from data.demo import load_demo_data
@@ -27,6 +28,15 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost",
+                   "https://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(BBBException)
 async def service_exception_handler(_: Request, exc: BBBException) -> Response:
